5ef7ae8021859fd4d23c492a40ae2ee0c850892f
p.dekterev <p.dekterev@opti-com.ru>
2020-09-02 19:10:15 +0300

    add Welcome component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Привет, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+const element = <Welcome name="Алиса" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
