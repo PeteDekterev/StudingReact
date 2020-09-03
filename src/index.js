5f06fc2a6537d608f52d6dc792637c334284553f
p.dekterev <p.dekterev@opti-com.ru>
2020-09-03 11:30:54 +0300

    new component

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Clock from './components/Clock';
 import * as serviceWorker from './serviceWorker';
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  // добавляем методы жизненного цикла
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Привет, мир!</h1>
-        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <Clock />,
