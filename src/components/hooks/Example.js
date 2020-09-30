5b1f61e2152d67e9c18195bf5537bc6748ba5041
p.dekterev <p.dekterev@opti-com.ru>
2020-09-30 13:46:25 +0300

    add comment & onClickBtn to Example

--- src/components/hooks/Example.js
+++ src/components/hooks/Example.js
@@ -3,6 +3,11 @@ import React, { useState, useEffect  } from 'react';
 function Example() {
   // Объявление переменной состояния, которую мы назовём "count"
   const [count, setCount] = useState(0);
+  // Такой синтаксис называется «деструктуризацией массивов (array destructuring)»
+  // Следующая запись аналогична:
+  // var countStateVars = useState(0); // Возвращает пару значений
+  // var count = countStateVars[0]; // Извлекаем первое значение
+  // var setCount = countStateVars[1]; // Извлекаем второе значение
 
   // По принципу componentDidMount и componentDidUpdate:
   useEffect(() => {
@@ -10,10 +15,14 @@ function Example() {
     document.title = `Вы нажали ${count} раз`;
   });
 
+  const onClickBtn = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div>
       <p>Вы нажали {count} раз</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={onClickBtn}>
         Нажми на меня
       </button>
     </div>
