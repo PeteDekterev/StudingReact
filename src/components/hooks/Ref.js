9ba835ab9d78f24697d8a3c019519cfa6032ad2e
p.dekterev <p.dekterev@opti-com.ru>
2020-10-01 13:36:05 +0300

    add hook to Ref

--- src/components/hooks/Ref.js
+++ src/components/hooks/Ref.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect} from 'react';
-
-let renderCount = 0;
+import React, { useState, useEffect, useRef} from 'react';
 
 function Ref() {
   const [value, setValue] = useState(0);
+  const renderCount = useRef(1);
 
   useEffect(() => {
-    renderCount++
+    renderCount.current++
   });
 
   return (
     <div>
-      <h1>Количество рендеров: {renderCount}</h1>
+      <h1>Количество рендеров: {renderCount.current}</h1>
       <input type="text" onChange={e => setValue(e.target.value)} value={value}/>
     </div>
   );
